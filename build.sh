92664291c69634a0ba515b71b2d08d6a3ae42d27
Joshua John <grantwaresng@gmail.com>
2026-08-30 11:55:26 +0100

    Fix Vercel Flutter web build

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 /tmp/flutter
+export PATH="$PATH:/tmp/flutter/bin"
+flutter config --enable-web
+flutter pub get
+flutter build web --release \
+  --dart-define=SUPABASE_URL=$SUPABASE_URL \
+  --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY \
+  --dart-define=PAYSTACK_PUBLIC_KEY=$PAYSTACK_PUBLIC_KEY \
+  --dart-define=FLUTTERWAVE_PUBLIC_KEY=$FLUTTERWAVE_PUBLIC_KEY \
+  --dart-define=PAYSTACK_SECRET_KEY=$PAYSTACK_SECRET_KEY
